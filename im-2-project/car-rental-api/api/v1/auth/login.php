3213df16a9135d757bb4e53b587784220003fc70
MiracleMark35 <markianfrost123@gmail.com>
2025-07-19 20:54:49 +0800

    Ayos

--- im-2-project/car-rental-api/api/v1/auth/login.php
+++ im-2-project/car-rental-api/api/v1/auth/login.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+///hellloooooo
 // Handle preflight
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
